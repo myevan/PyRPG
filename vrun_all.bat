3453e3c349c7e42ba2cf689858b63e364444ac42
Song Young-Jin <myevan@outlook.com>
2022-12-18 21:40:08 +0900

    vrun_all.bat: core.data.table

--- vrun_all.bat
+++ vrun_all.bat
@@ -1,2 +1,3 @@
-start /b vcli.bat gspread-csv zone.proto --out=temps
-start /b vpython.bat -m examples.ex01.main
+call vpython.bat -m core.data.table
+call vpython.bat -m examples.ex01.main
+call vcli.bat --debug gspread-csv zone.proto --out=temps
