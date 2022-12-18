ea174594163fad9adb2f704602cb881650ab2e33
myevan <myevan@outlook.com>
2022-12-18 16:37:24 +0000

    core.data.table refactoring

--- core/data/table.py
+++ core/data/table.py
@@ -39,10 +39,6 @@ class FieldEnum:
         return get(key)
 
 class FieldType:
-    class Error(Exception):
-        def __init__(self, name, col):
-            super(FieldType.Error, self).__init__(name)
-            self.col = col
     """
     data_type:main_attr:sub_attr
     """
@@ -87,7 +83,7 @@ class FieldType:
     # 180: hashlib.md5
     # 179: hashlib.sha1
     # 403: hashlib.sha256
-    expr_to_parse_text = {
+    expr_to_convert = {
         'str:pk:hash64': lambda t, m, s: hash(t),
         'str:pk:hash32': lambda t, m, s: hash(t),
         'str:pk:adler32': lambda t, m, s: adler32(t.encode('utf8')),
@@ -99,7 +95,7 @@ class FieldType:
         'str:ascii:replace': lambda t, m, s: t.encode('ascii', 'replace'),
     }
 
-    py_type2_to_parse_text = {
+    py_type_opt_to_convert = {
         (int, 'bin'): lambda t, m, s: int(t, 2),
         (int, 'oct'): lambda t, m, s: int(t, 8),
         (int, 'hex'): lambda t, m, s: int(t, 16),
@@ -109,8 +105,8 @@ class FieldType:
         (str, 'fk'): lambda t, m, s: adler32(t.encode('utf8')),
     }
 
-    py_type_to_parse_text = {
-        str: lambda t, m, s: t.encode(m) if m else t.encode('utf8'),
+    py_type_to_convert = {
+        str: lambda t, m, s: t,
         int: lambda t, m, s: int(t, int(m)) if m else int(t),
         float: lambda t, m, s: float(t),
         date: lambda t, m, s: datetime.strptime(t, "%Y-%m-%d").date(),
@@ -126,12 +122,12 @@ class FieldType:
     }
 
     @classmethod
-    def add(cls, data_type_name, py_type, c_type, parse_text):
-        cls.expr_to_parse_text[data_type_name] = parse_text
+    def add(cls, data_type_name, py_type, c_type, convert):
+        cls.expr_to_convert[data_type_name] = convert
         cls.data_type_name_to_type_pair[data_type_name] = (py_type, c_type)
 
     @classmethod
-    def parse_expr(cls, expr: str):
+    def parse(cls, expr: str):
         mo = cls.ro_field_type_expr.match(expr)
         if mo:
             data_type_name = mo.group(1)
@@ -139,94 +135,116 @@ class FieldType:
             sub_attr = mo.group(5)
             return cls(expr, data_type_name, main_attr, sub_attr)
 
-    @classmethod
-    def gen_field_types(cls, exprs: list):
-        for idx, expr in enumerate(exprs):
-            try:
-                yield cls.parse_expr(expr)
-            except ValueError as exc:
-                raise cls.Error(f"{str(exc)} COL: {idx}", col=idx)
-
     def __init__(self, expr, data_type_name, main_attr, sub_attr):
         type_pair = self.expr_to_type_pair.get(expr, None)
         if not type_pair:
             type_pair = self.data_type_name_to_type_pair.get(data_type_name, None)
             if not type_pair:
-                raise ValueError(f"UNKNOWN_DATA_TYPE: {data_type_name} EXPR: {expr}")
+                raise ValueError(f"UNKNOWN_DATA_TYPE={data_type_name} EXPR={expr}")
 
-        parse_text = self.expr_to_parse_text.get(expr)
-        if not parse_text:
+        convert = self.expr_to_convert.get(expr)
+        if not convert:
             py_type = type_pair[0]
-            parse_text = self.py_type2_to_parse_text.get((py_type, main_attr), None)
-            if not parse_text:
-                parse_text = self.py_type_to_parse_text[py_type]
+            convert = self.py_type_opt_to_convert.get((py_type, main_attr), None)
+            if not convert:
+                convert = self.py_type_to_convert[py_type]
 
         c_type = type_pair[1]
-        dump_value = self.type_pair_to_dump.get(type_pair, c_type)
+        dump = self.type_pair_to_dump.get(type_pair, c_type)
 
         self._type_pair = type_pair
         self._sub_attr = sub_attr
         self._main_attr = main_attr
         self._data_type_name = data_type_name
-        self._parse_text = parse_text
-        self._dump_value = dump_value
+        self._convert = convert
+        self._dump = dump
         self._expr = expr
 
     def __repr__(self):
         return self._expr
 
-    def parse_text(self, text):
-        return self._parse_text(text, self._main_attr, self._sub_attr)
+    def convert(self, val):
+        return self._convert(val, self._main_attr, self._sub_attr)
 
-    def dump(self, value):
-        return bytes(self._dump_value(value))
+    def dump(self, val):
+        return bytes(self._dump(val))
 
 class Table:
-    @classmethod
-    def parse(cls, rows):
-        field_names = rows[0]
-        field_types = list(FieldType.gen_field_types(rows[1]))
+    class Error(Exception):
+        def __init__(self, name, row, col, memo):
+            super(Table.Error, self).__init__(name)
+            self.name = name
+            self.col = col
+            self.row = row
+            self.memo = memo
 
-        def gen_records():
-            def gen_values(texts):
-                for col_idx, (field_type, text) in enumerate(zip(field_types, texts)):
-                    yield field_type.parse_text(text)
+        def __str__(self):
+            return f"{self.name}<ROW={self.row} COL={self.col} {self.memo}>"
 
-            for row_idx in range(ROW_IDX_BODYS, len(rows)):
-                yield list(gen_values(rows[row_idx]))
-
-        records = list(gen_records())
-        return cls(field_names, field_types, rows[ROW_IDX_BODYS:])
-
-    def __init__(self, field_names, field_types, records):
-        self._field_names = field_names
-        self._field_types = field_types
-        self._records = records
+    def __init__(self, fld_names, fld_types, recs):
+        self._fld_names = fld_names
+        self._fld_types = fld_types
+        self._recs = recs
 
     def __repr__(self):
-        head_line = ', '.join(self._field_names)
-        type_line = ', '.join(repr(field_type) for field_type in self._field_types)
+        head_line = ', '.join(repr(fld_name) for fld_name in self._fld_names)
+        type_line = ', '.join(repr(fld_type) for fld_type in self._fld_types)
         body_lines = [
-            ', '.join(repr(field_value) for field_value in record)
-                for record in self._records]
+            ', '.join(repr(fld_value) for fld_value in record)
+                for record in self._recs]
         return '\n'.join([head_line, type_line] + body_lines)
 
     @property
-    def field_names(self): return self._field_names
+    def field_names(self): return self._fld_names
 
     @property
-    def field_types(self): return self._field_types
+    def field_types(self): return self._fld_types
 
     @property
-    def records(self): return self._records
+    def records(self): return self._recs
+
+    @property
+    def rows(self):
+        yield self._fld_names
+        yield self._fld_types
+        for rec in self._recs:
+            yield rec
+
+class PyTable(Table):
+    @classmethod
+    def create(cls, rows: list):
+        def gen_field_types(row_idx, exprs: list):
+            for col_idx, expr in enumerate(exprs):
+                try:
+                    yield FieldType.parse(expr)
+                except ValueError as exc:
+                    raise cls.Error(f"FIELD_TYPE_EXPR_ERROR", row=row_idx, col=col_idx, memo=str(exc))
+
+        def gen_field_values(fld_types, fld_vals):
+            for col_idx, (fld_type, fld_val) in enumerate(zip(fld_types, fld_vals)):
+                yield fld_type.convert(fld_val)
+
+        def gen_records(fld_types, rowi):
+            for row_idx, fld_vals in enumerate(rowi):
+                yield list(gen_field_values(fld_types, fld_vals))
+
+        rowi = iter(rows)
+        fld_names = next(rowi)
+        fld_exprs = next(rowi)
+        fld_types = list(gen_field_types(ROW_IDX_TYPES, fld_exprs))
+        recs = list(gen_records(fld_types, rowi))
+        return cls(fld_names, fld_types, recs)
+
 
 class L10NBinaryTable(Table):
     RO_FIELD_NAME = re.compile('\$(\w+)\[(\w+)\]')
 
     @classmethod
     def create(cls, org_table):
-        rows = cls.gen_rows(org_table.field_names, org_table.records)
-        return cls(rows[ROW_IDX_HEADS], rows[ROW_IDX_TYPES], rows[ROW_IDX_BODYS:])
+        rowi = cls.gen_rows(org_table.field_names, org_table.records)
+        heads = next(rowi)
+        types = next(rowi)
+        return cls(heads, types, list(rowi))
 
     @classmethod
     def gen_rows(cls, field_names, records):
@@ -234,37 +252,39 @@ class L10NBinaryTable(Table):
 
         key_head_text = 'key'.encode('utf8')
         key_head_hash = adler32(key_head_text)
-        yield b'head', b'hash', b'text'
-        yield b'int', b'int', b'str:utf8'
+        yield 'head', 'hash', 'text'
+        yield 'int', 'int', 'str:utf8'
         yield 0, 0, b'adler32'
         yield 0, key_head_hash, key_head_text
         for val_idx, mo in enumerate(mos):
             if mo:
                 field_name = mo.group(1)
-                locale_head_text = mo.group(2).encode('utf8')
-                locale_head_hash = adler32(locale_head_text)
+                locale_head_text = mo.group(2)
+                locale_head_hash = adler32(locale_head_text.encode('utf8'))
                 yield 0, locale_head_hash, locale_head_text
                 key_idx = field_names.index(field_name)
                 for record in records:
-                    key_text = record[key_idx].encode('utf8')
-                    locale_text = record[val_idx].encode('utf8')
-                    key_text_hash = adler32(key_text)
+                    key_text = record[key_idx]
+                    locale_text = record[val_idx]
+                    key_text_hash = adler32(key_text.encode('utf8'))
                     yield key_head_hash, key_text_hash, key_text
                     yield locale_head_hash, key_text_hash, locale_text
 
 class L10NTextTable(Table):
     @classmethod
     def create(cls, org_table):
-        rows = cls.gen_rows(org_table.field_names, org_table.records)
-        return cls(rows[0], rows[1], rows[2:])
+        rowi = cls.gen_rows(org_table.field_names, org_table.records)
+        heads = next(rowi)
+        types = next(rowi)
+        return cls(heads, types, list(rowi))
 
     @classmethod
     def gen_rows(cls, field_names, records):
         rows = list(L10NBinaryTable.gen_rows(field_names, records))
 
         texts = defaultdict(OrderedDict)
-        for head, hash, bin in rows[ROW_IDX_BODYS:]:
-            texts[head][hash] = bin.decode('utf8')
+        for head, hash, text in rows[ROW_IDX_BODYS:]:
+            texts[head][hash] = text
 
         head_texts = list(text for text in texts[0].values())
         yield head_texts
@@ -279,61 +299,90 @@ class L10NTextTable(Table):
             assert(all(pair[0] == hash for pair in pairs))
             yield [hash] + [pair[1] for pair in pairs]
 
-    
+class CompactTable(Table):
+    RO_FIELD_NAME = re.compile("\w+")
+
+    @classmethod
+    def create(cls, org_table):
+        rowi = cls.gen_rows(org_table.field_names, org_table.field_types, org_table.records)
+        heads = next(rowi)
+        types = next(rowi)
+        return cls(heads, types, rowi)
+
+    @classmethod
+    def gen_rows(cls, field_names, field_types, records):
+        mos = [cls.RO_FIELD_NAME.match(name) for name in field_names]
+        idxs = [idx for idx, mo in enumerate(mos) if mo]
+
+        yield [field_names[idx] for idx in idxs]
+        yield [field_types[idx] for idx in idxs]
+
+        for record in records:
+            if not record: continue
+            if type(record[0]) is str:
+                if not record[0].strip(): continue # empty record
+                if record[0].startswith('#'): continue # comment record
+
+            row = [record[idx] for idx in idxs]
+            if type(row[0]) is str:
+                if not row[0].strip(): continue # empty row
+                if row[0].startswith('#'): continue # comment row
+
+            yield row
 
 if __name__ == '__main__':
     import logging
     FieldEnum.add("logging", lambda key: getattr(logging, key))
 
     import json
-    FieldType.add("json", str, bytes, parse_text=lambda t, m, s: json.loads(t))
+    FieldType.add("json", str, bytes, convert=lambda t, m, s: json.loads(t))
 
     import yaml
-    FieldType.add("yaml", str, bytes, parse_text=lambda t, m, s: yaml.safe_load(t))
+    FieldType.add("yaml", str, bytes, convert=lambda t, m, s: yaml.safe_load(t))
 
-    int_type = FieldType.parse_expr("int")
-    int_bin_type = FieldType.parse_expr("int:bin")
-    int_oct_type = FieldType.parse_expr("int:oct")
-    int_hex_type = FieldType.parse_expr("int:hex")
-    int_pk_type = FieldType.parse_expr("int:pk")
-    int16_pk_type = FieldType.parse_expr("int16:pk")
-    int_fk_type = FieldType.parse_expr("int:fk:User.id")
-    str_pk_type = FieldType.parse_expr("str:pk")
-    str_pk_md5_type = FieldType.parse_expr("str:pk:md5")
-    str_type = FieldType.parse_expr("str")
-    str_ascii_type = FieldType.parse_expr("str:ascii")
-    str_ascii_replace_type = FieldType.parse_expr("str:ascii:replace")
-    str_utf8_type = FieldType.parse_expr("str:utf8")
-    str_utf16_type = FieldType.parse_expr("str:utf16")
-    date_type = FieldType.parse_expr("date")
-    time_type = FieldType.parse_expr("time")
-    span_type = FieldType.parse_expr("span")
-    json_type = FieldType.parse_expr("json")
-    yaml_type = FieldType.parse_expr("yaml")
-    real_type = FieldType.parse_expr("real")
-    enum_type = FieldType.parse_expr("enum:logging")
+    int_type = FieldType.parse("int")
+    int_bin_type = FieldType.parse("int:bin")
+    int_oct_type = FieldType.parse("int:oct")
+    int_hex_type = FieldType.parse("int:hex")
+    int_pk_type = FieldType.parse("int:pk")
+    int16_pk_type = FieldType.parse("int16:pk")
+    int_fk_type = FieldType.parse("int:fk:User.id")
+    str_pk_type = FieldType.parse("str:pk")
+    str_pk_md5_type = FieldType.parse("str:pk:md5")
+    str_type = FieldType.parse("str")
+    str_ascii_type = FieldType.parse("str:ascii")
+    str_ascii_replace_type = FieldType.parse("str:ascii:replace")
+    str_utf8_type = FieldType.parse("str:utf8")
+    str_utf16_type = FieldType.parse("str:utf16")
+    date_type = FieldType.parse("date")
+    time_type = FieldType.parse("time")
+    span_type = FieldType.parse("span")
+    json_type = FieldType.parse("json")
+    yaml_type = FieldType.parse("yaml")
+    real_type = FieldType.parse("real")
+    enum_type = FieldType.parse("enum:logging")
 
-    print(int_type.parse_text("10"))
-    print(int_bin_type.parse_text("10"))
-    print(int_oct_type.parse_text("10"))
-    print(int_hex_type.parse_text("10"))
-    print(int_pk_type.parse_text("100"))
-    print(int16_pk_type.parse_text("30000"))
-    print(int_fk_type.parse_text("10000000000"))
-    print(str_pk_type.parse_text("Hello:PK"))
-    print(str_pk_md5_type.parse_text("Hello:PK:MD5"))
-    print(str_type.parse_text("Hello"))
-    print(str_ascii_type.parse_text("Hello:ASCII"))
-    print(str_ascii_replace_type.parse_text("Hello:ASCII:REPLACE:안녕하세요"))
-    print(str_utf8_type.parse_text("안녕하세요"))
-    print(str_utf16_type.parse_text("안녕하세요"))
-    print(date_type.parse_text("2022-12-18"))
-    print(time_type.parse_text("11:50:07"))
-    print(span_type.parse_text("01:20:30"))
-    print(json_type.parse_text("[1, 2, 3]"))
-    print(yaml_type.parse_text("{a: 1, b: 2}"))
-    print(real_type.parse_text("1.23"))
-    print(enum_type.parse_text("ERROR"))
+    print(int_type.convert("10"))
+    print(int_bin_type.convert("10"))
+    print(int_oct_type.convert("10"))
+    print(int_hex_type.convert("10"))
+    print(int_pk_type.convert("100"))
+    print(int16_pk_type.convert("30000"))
+    print(int_fk_type.convert("10000000000"))
+    print(str_pk_type.convert("Hello:PK"))
+    print(str_pk_md5_type.convert("Hello:PK:MD5"))
+    print(str_type.convert("Hello"))
+    print(str_ascii_type.convert("Hello:ASCII"))
+    print(str_ascii_replace_type.convert("Hello:ASCII:REPLACE:안녕하세요"))
+    print(str_utf8_type.convert("안녕하세요"))
+    print(str_utf16_type.convert("안녕하세요"))
+    print(date_type.convert("2022-12-18"))
+    print(time_type.convert("11:50:07"))
+    print(span_type.convert("01:20:30"))
+    print(json_type.convert("[1, 2, 3]"))
+    print(yaml_type.convert("{a: 1, b: 2}"))
+    print(real_type.convert("1.23"))
+    print(enum_type.convert("ERROR"))
 
     print(int_type.dump(10).hex())
     print(real_type.dump(1.23).hex())
@@ -341,17 +390,19 @@ if __name__ == '__main__':
     print(date_type.dump(datetime.now()))
     print(time_type.dump(timedelta(hours=1, minutes=2, seconds=3)))
 
-    org_table = Table.parse([
-        ['id', 'name', '$name[src]', 'desc', '$desc[src]'],
-        ['int', 'str:fk:string.key', 'str:utf8', 'str:fk:string.key', 'str:utf8'],
-        ['1', 'NAME_A', '가 이름', 'DESC_A', '가 설명'],
-        ['2', 'NAME_B', '나 이름', 'DESC_B', '나 설명'],
+    org_table = PyTable.create([
+        ["id",      "name",     "$name[src]",   "desc",     "$desc[src]"   "ave_score",     "#comment"],
+        ["int:pk",  "str",      "str:utf8",     "str",      "str:utf8",    "real",          "str:utf8"],
+        ["1",       "NAME_A",   "가 이름",      "DESC_A",   "가 설명",      7.2,            "주석1"],
+        ["2",       "NAME_B",   "나 이름",      "DESC_B",   "나 설명",      8.5,            "주석2"],
     ])
     print(repr(org_table))
 
-    for head, hash, bin in L10NBinaryTable.gen_rows(org_table.field_names, org_table.records):
-        row = [head, hash, bin.decode('utf8')] 
+    for row in L10NBinaryTable.create(org_table).rows:
         print(row)
 
-    for row in L10NTextTable.gen_rows(org_table.field_names, org_table.records):
+    for row in L10NTextTable.create(org_table).rows:
+        print(row)
+
+    for row in CompactTable.create(org_table).rows:
         print(row)
